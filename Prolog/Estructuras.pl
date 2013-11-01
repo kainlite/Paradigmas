5baec0fba01bd844ac6e4606b2073d76eaab30a6
kainlite <kainlite@gmail.com>
2013-10-31 21:05:50 -0300

    Updating..

--- Prolog/Estructuras.pl
+++ Prolog/Estructuras.pl
@@ -10,48 +10,72 @@ parejas(L, [], L).
 parejas([H1|T1], [H2|T2], L3):- size(T1, N1), size(T2, N2), N1 == N2, parejas(T1, T2, L2), L3 = [[H1,H2]|L2].
 
 % 02 
-superior(pers(r,31235630,29),pers(a,30123159,30)).
-superior(pers(r,31235630,29),pers(b,20200123,40)).
-superior(pers(r,31235630,29),pers(c,10200369,60)).
-superior(pers(a,30123159,30),pers(d,26555106,50)).
-superior(pers(a,30123159,30),pers(e,6333222,70)).
-superior(pers(e,6333222,70),pers(h,3000222,30)).
-superior(pers(b,20200123,40),pers(g,20203203,30)).
-superior(pers(b,20200123,40),pers(f,6666333,20)).
-superior(pers(f,6666333,20),pers(i,9666222,80)).
-superior(pers(f,6666333,20),pers(j,10111336,60)).
-superior(pers(g,20203203,30),pers(k,11002333,30)).
+persona(r,1, 2).
+persona(a,2, 3).
+persona(b,3, 4).
+persona(c,4, 5).
+persona(d,5, 6).
+persona(e,6, 7).
+persona(f,7, 8).
+persona(g,8, 9).
+persona(h,9, 10).
+persona(i,10, 11).
+persona(j,11, 12).
+persona(k,12, 13).
+padre(r,a).
+padre(r,b).
+padre(r,c).
+padre(a,d).
+padre(a,e).
+padre(b,f).
+padre(b,g).
+padre(e,h).
+padre(f,i).
+padre(f,j).
+padre(g,k).
+hijo(X,Y):-padre(Y,X).
 
-desc(X,Y):- superior(pers(X,_,_),pers(Y,_,_)).
-asc(X,Y):- superior(pers(Y,_,_),pers(X,_,_)).
+descendientes(X,Y):-padre(X,Y).
+descendientes(X,Y):-padre(X,Z), descendientes(Z,Y).
 
-% 03 
-persona(nombre(juan),tel(4215556),sexo(m),edad(22)).
-persona(nombre(pedro),tel(4336688),sexo(m),edad(50)).
-persona(nombre(laura),tel(4215556),sexo(f),edad(21)).
-visita(nombre(juan),nombre(laura)).
+ascendientes(X,Y):-padre(Y,X).
+ascendientes(X,Y):-padre(Y,Z), ascendientes(X,Z).
 
-existe(X):- persona(nombre(X),_,_,_).
-todas(X):-persona(X,_,_,_).
-mujeres(X):- persona(nombre(X),_,sexo(f),_).
-mayores(X):- persona(nombre(X),_,sexo(m),edad(Y)), Y > 21.
-casados(X,Y):- persona(X,tel(S),_,_) , persona(Y,tel(S),_,_).
+% 03
+persona2(a, 123, male, 38).
+persona2(b, 123, female, 23).
+persona2(c, 345, male, 33).
+persona2(d, 355, female, 34).
+persona2(e, 395, male, 16).
+buscarPers(X):- persona2(X, _, _,_).
+mostrarMujeres(X):- persona2(X,_,female,_).
+mostrarMayores(Y):- persona2(Y,_,male,R), R > 21.
+casado(a,b).
+casado(c,d).
+casadosTel(X,Y):- casado(X,Y), persona2(X,T1,_,_), persona2(Y,T2,_,_), T1 == T2.
+visita(a,e).
+visita(b,d).
+visita(X,Y):- visita(Y,X).
+
+% es la relacion de cachos.
+% c(X):- if casado(X, Y), visita(Y, Z), persona2(Z, _, %m).
 
 % 04
-ruta(sanjuan,cordoba,480).
-ruta(cordoba,sanluis,350).
-ruta(sanluis,bsas,710).
-ruta(cordoba,stafe,280).
-ruta(cordoba,misiones,480).
-ruta(cordoba,bsas,540).
-ruta(misiones,stafe,570).
-ruta(mza,sanjuan,160).
-ruta(mza,sanluis,280).
+ruta(mendoza, sanluis, 280).
+ruta(sanjuan, cordoba, 480).
+ruta(cordoba, sanjuan, 350).
+ruta(sanluis, bsas, 710).
+ruta(cordoba, bsas, 540).
+ruta(cordoba, misiones, 950).
+ruta(cordoba, santafe, 280).
+ruta(santafe, misiones, 570).
+ruta(mendoza, sanjuan,160).
+camino(X, Y, Z):- ruta(X, Y, Z).
+camino(X, Y, Z):- ruta(X, C2, R1), camino(C2, Y, R2), Z is R1 + R2.   
+
+camino2(X, Y):- ruta(X, Y, _).
+camino2(X, Y):- ruta(X, C2, _), camino2(C2, Y), !.  
 
 
-camino(X,Y):-ruta(X,Y,_).
-
-dist(X,Y,D):-ruta(X,Y,D).
-dist(X,Y,D):-ruta(X,Z,D1),dist(Z,Y,D2), D is D1+D2.
-mostrar(X,Y):-ruta(X,Y,_).
-mostrar(X,Y):-ruta(X,Z,_),mostrar(Z,Y),write(Z),write(' ').
+camino3(X, Y, Z):- ruta(X, Y, Z).
+camino3(X, Y, _):- ruta(X, C2, _), camino3(C2, Y, _), write(C2), write(' '), !.   
